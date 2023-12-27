af93cef2d0e4888f23799e4eb0a44b07b71e63a7
Harishraj16 <harishselvaraj27@gmail.com>
2023-12-27 17:52:41 +0530

    Edited input_validation.js

--- Basics/middlewares/input_validation.js
+++ Basics/middlewares/input_validation.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const zod = require('zod');
 const app = express();
+
+const schema = zod.array(zod.number());
 
 app.use(express.json());
 
 app.post('/movies-count', function(req, res){
     const movies = req.body.movies;
-    const numofmovies = movies.length;
-    
-    res.send("You watched "+ numofmovies + " movies!");
+    const response = schema.safeParse(movies);
+    if(!response.success){
+        res.status(411).json({
+            msg: "input is invalid"
+        })
+    }else {
+        res.send({
+            response
+        })
+    }
 });
 
 app.listen(3000,()=>{
